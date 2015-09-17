3e995ad84985d1bce33a1ca37d8f4575d6348ccc
Jenna Zeigen <jenna.zeigen@gmail.com>
2015-09-17 10:18:32 -0400

    working db setup w00t

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150819202200) do
+ActiveRecord::Schema.define(version: 20150910211515) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "humans", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "kitty_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "kitties", force: :cascade do |t|
     t.string   "name"
